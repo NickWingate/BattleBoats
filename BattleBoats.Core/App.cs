53b987177074f9e6188e563b3fe3da358c80b81a
NickWingate <nicholas.wingate03@gmail.com>
2020-11-29 23:07:45 +0000

    Add project files.

---
+++ BattleBoats.Core/App.cs
@@ -0,0 +1,16 @@
+﻿using BattleBoats.Core.ViewModels;
+using MvvmCross.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BattleBoats.Core
+{
+    public class App : MvxApplication
+    {
+        public override void Initialize()
+        {
+            RegisterAppStart<MenuViewModel>();
+        }
+    }
+}
---
+++ BattleBoats.Core/ViewModels/MenuViewModel.cs
@@ -0,0 +1,11 @@
+﻿using MvvmCross.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BattleBoats.Core.ViewModels
+{
+    public class MenuViewModel : MvxViewModel
+    {
+    }
+}
