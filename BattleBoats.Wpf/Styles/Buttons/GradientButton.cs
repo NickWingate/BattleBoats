2c6016c02f4ceb106d5cec472d4e7eae91b340a8
NickWingate <nicholas.wingate03@gmail.com>
2020-12-05 00:19:42 +0000

    Renamed GradientButton styles, made fine ajustments to style

---
+++ BattleBoats.Wpf/Styles/Buttons/GradientButton.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Effects;
+
+
+namespace BattleBoats.Wpf.Styles.Buttons
+{
+    public class GradientButton : Button
+    {
+        public Color GradientColor0
+        {
+            get { return (Color)GetValue(GradientColor0Property); }
+            set { SetValue(GradientColor0Property, value); }
+        }
+        public Color GradientColor1
+        {
+            get { return (Color)GetValue(GradientColor1Property); }
+            set { SetValue(GradientColor1Property, value); }
+        }
+        public Color GradientColor2
+        {
+            get { return (Color)GetValue(GradientColor2Property); }
+            set { SetValue(GradientColor2Property, value); }
+        }
+
+        public static readonly DependencyProperty GradientColor0Property =
+            DependencyProperty.Register("GradientColor0", typeof(Color), typeof(GradientButton), new PropertyMetadata((sender, args) => {
+                System.Diagnostics.Debug.WriteLine("Changed color 0");
+            }));
+        public static readonly DependencyProperty GradientColor1Property =
+            DependencyProperty.Register("GradientColor1", typeof(Color), typeof(GradientButton), new PropertyMetadata((sender, args) => {
+                System.Diagnostics.Debug.WriteLine("Changed color 1");
+            }));
+        public static readonly DependencyProperty GradientColor2Property =
+            DependencyProperty.Register("GradientColor2", typeof(Color), typeof(GradientButton), new PropertyMetadata((sender, args) => {
+                System.Diagnostics.Debug.WriteLine("Changed color 2");
+            }));
+
+        public GradientButton() : base()
+        {
+            this.DefaultStyleKey = typeof(GradientButton);
+        }
+    }
+}
