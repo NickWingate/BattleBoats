6279f03c50401309337878e2bd30304848840a25
Nick Wingate <nicholas.wingate03@gmail.com>
2020-12-05 22:57:30 +0000

    Added IShipSet interface, and coordinate class

---
+++ BattleBoats.Wpf/Models/Coordinate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BattleBoats.Wpf.Models
+{
+    public class Coordinate
+    {
+        public int XCoord { get; set; }
+        public int YCoord { get; set; }
+        public Coordinate(int xCoord, int yCoord)
+        {
+            XCoord = xCoord;
+            YCoord = yCoord;
+        }
+        public override string ToString()
+        {
+            return $"{XCoord}, {YCoord}";
+        }
+    }
+}
---
+++ BattleBoats.Wpf/Models/IShipSet.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BattleBoats.Wpf.Models
+{
+    public interface IShipSet
+    {
+        public string Name { get; set; }
+        public bool Sunk { get; set; }
+        public Coordinate Location { get; set; }
+        public int Length { get; set; }
+        public int Health { get; set; }
+    }
+}
