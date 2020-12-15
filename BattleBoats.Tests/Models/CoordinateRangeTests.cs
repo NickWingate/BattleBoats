6d56087c2497e6b38abe2d11d6ceb2aeac563ba9
NickWingate <nicholas.wingate03@gmail.com>
2020-12-15 01:08:51 +0000

    Added unit tests for board validation

---
+++ BattleBoats.Tests/Models/CoordinateRangeTests.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BattleBoats.Wpf.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Reflection;
+
+namespace BattleBoats.Wpf.Models.Tests
+{
+    [TestClass()]
+    public class CoordinateRangeTests
+    {
+        [TestMethod()]
+        public void GetAllCoordinatesTest()
+        {
+            // Arrange
+            CoordinateRange coordinateRange = new CoordinateRange(
+                new Coordinate(0, 0),
+                new Coordinate(3, 0));
+            List<Coordinate> expected = new List<Coordinate>() 
+            {
+                new Coordinate(0, 0),
+                new Coordinate(1, 0),
+                new Coordinate(2, 0),
+                new Coordinate(3, 0),
+            };
+
+            // Act
+            List<Coordinate> actual = coordinateRange.GetAllCoordinates();
+
+            // Assert
+
+            // Check Lists are same length
+            Assert.AreEqual(expected.Count, actual.Count);
+            // Check each item in lists
+            for (var i = 0; i < expected.Count; i++)
+            {
+                // Get properties of each item
+                PropertyInfo[] expectedProps = expected.GetType().GetProperties();
+                PropertyInfo[] actualProps = actual.GetType().GetProperties();
+                Assert.AreEqual(expectedProps.Length, actualProps.Length);
+                // Compare properties of each item
+                for (int j = 0; j < expectedProps.Length; j++)
+                {
+                    Assert.AreEqual(expectedProps[j], actualProps[j]);
+                }
+            }
+        }
+    }
+}
---
+++ BattleBoats.Tests/ViewModels/BoatPlacementViewModelTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BattleBoats.Wpf.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using BattleBoats.Wpf.Services.Navigation;
+using BattleBoats.Wpf.Models;
+
+namespace BattleBoats.Wpf.ViewModels.Tests
+{
+    [TestClass()]
+    public class BoatPlacementViewModelTests
+    {
+        [TestMethod()]
+        public void ValidBoatPlacementTest()
+        {
+            BoatPlacementViewModel boatPlacementViewModel= new BoatPlacementViewModel(new Navigator());
+            boatPlacementViewModel.Boats = new List<IBoat>
+            {
+                new Boat(0, 0, 3, 9),
+                new Boat(3, 3, 2, 9),
+                //new Boat(0, 0, 3, 9),
+            };
+
+            bool expected = true;
+            bool acctual = boatPlacementViewModel.ValidBoatPlacement;
+            
+            Assert.AreEqual(expected, acctual);
+        }
+    }
+}
