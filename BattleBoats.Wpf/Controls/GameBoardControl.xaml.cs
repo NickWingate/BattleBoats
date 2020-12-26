3dcab5ff0db839df8935dc87e1c6d6034c089a12
NickWingate <nicholas.wingate03@gmail.com>
2020-12-26 18:51:29 +0000

    Dynamic boat control generation in GameBoardControl

--- BattleBoats.Wpf/Controls/GameBoardControl.xaml.cs
+++ BattleBoats.Wpf/Controls/GameBoardControl.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using BattleBoats.Wpf.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.Text;
 using System.Threading;
@@ -21,7 +22,7 @@ namespace BattleBoats.Wpf.Controls
     /// <summary>
     /// Interaction logic for GameBoard.xaml
     /// </summary>
-    public partial class GameBoardControl : Grid
+    public partial class GameBoardControl : Grid, INotifyPropertyChanged
     {
         public GameBoardControl()
         {
@@ -37,7 +38,8 @@ namespace BattleBoats.Wpf.Controls
         }
         // Using a DependencyProperty as the backing store for Boats.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty BoatsProperty =
-            DependencyProperty.Register(nameof(Boats), typeof(List<IBoat>), typeof(GameBoardControl), new PropertyMetadata(null));
+            DependencyProperty.Register(nameof(Boats), typeof(List<IBoat>), typeof(GameBoardControl), 
+                new PropertyMetadata(new List<IBoat> { new Boat(0, 0, 0, 0) { ShowItem = false} }, OnBoatsPropertyChanged));
 
 
         public IGameItem Target
@@ -50,10 +52,18 @@ namespace BattleBoats.Wpf.Controls
         public static readonly DependencyProperty TargetProperty =
             DependencyProperty.Register(nameof(Target), typeof(IGameItem), typeof(GameBoardControl), new PropertyMetadata(null));
 
+        private static void OnBoatsPropertyChanged(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs e)
+        {
+            GameBoardControl gameBoardControl = dependencyObject as GameBoardControl;
+            gameBoardControl.OnPropertyChanged(nameof(Boats));
+            gameBoardControl.OnBoatsPropertyChanged(e);
+        }
 
-
-
-
+        private void OnBoatsPropertyChanged(DependencyPropertyChangedEventArgs e)
+        {
+            CreateBoats();
+        }
+       
 
         private void CreateBoard(int boardDimention)
         {
@@ -91,45 +101,79 @@ namespace BattleBoats.Wpf.Controls
         }
 
         // Try to create boats dynamically
-        //private void CreateBoats()
-        //{
-        //    for (int i = 0; i < 5; i++)
-        //    {
-        //        BoatControl boatControl = new BoatControl();
+        private void CreateBoats()
+        {
+            foreach (IBoat boat in Boats)
+            {
+                BoatControl boatControl = new BoatControl();
 
-        //        Binding height = new Binding(nameof(BoatControl.BoatHeightProperty));
-        //        height.Source = Boats[i].RowSpan;
+                Binding height = new Binding(nameof(BoatControl.BoatHeightProperty))
+                {
+                    Source = boat,
+                    Path = new PropertyPath("RowSpan")
+                };
 
-        //        Binding width = new Binding(nameof(BoatControl.BoatWidthProperty));
-        //        width.Source = boat.ColumnSpan;
+                Binding width = new Binding(nameof(BoatControl.BoatWidthProperty))
+                {
+                    Source = boat,
+                    Path = new PropertyPath("ColumnSpan")
+                };
 
-        //        Binding column = new Binding(nameof(ColumnProperty));
-        //        column.Source = boat.Column;
+                Binding column = new Binding(nameof(ColumnProperty))
+                {
+                    Source = boat,
+                    Path = new PropertyPath("Column")
+                };
 
-        //        Binding row = new Binding(nameof(RowProperty));
-        //        row.Source = boat.Row;
+                Binding row = new Binding(nameof(RowProperty)) 
+                {
+                    Source = boat,
+                    Path = new PropertyPath("Row")
+                };
 
-        //        Binding columnSpan = new Binding(nameof(ColumnSpanProperty));
-        //        columnSpan.Source = boat.ColumnSpan;
+                Binding columnSpan = new Binding(nameof(ColumnSpanProperty))
+                {
+                    Source = boat,
+                    Path = new PropertyPath("ColumnSpan")
+                };
 
-        //        Binding rowSpan = new Binding(nameof(RowSpanProperty));
-        //        rowSpan.Source = boat.RowSpan;
+                Binding rowSpan = new Binding(nameof(RowSpanProperty))
+                {
+                    Source = boat,
+                    Path = new PropertyPath("RowSpan")
+                };
 
-        //        Binding isSelected = new Binding(nameof(BoatControl.IsSelectedProperty));
-        //        isSelected.Source = boat.IsSelected;
+                Binding isSelected = new Binding(nameof(BoatControl.IsSelectedProperty))
+                {
+                    Source = boat,
+                    Path = new PropertyPath("IsSelected")
+                };
+
+                Binding isEnabled = new Binding(nameof(BoatControl.ShowBoat))
+                {
+                    Source = boat,
+                    Path = new PropertyPath("ShowItem")
+                };
 
 
-        //        boatControl.SetBinding(BoatControl.BoatHeightProperty, height);
-        //        boatControl.SetBinding(BoatControl.BoatWidthProperty, width);
-        //        boatControl.SetBinding(ColumnProperty, column);
-        //        boatControl.SetBinding(RowProperty, row);
-        //        boatControl.SetBinding(ColumnSpanProperty, columnSpan);
-        //        boatControl.SetBinding(RowSpanProperty, rowSpan);
-        //        boatControl.SetBinding(BoatControl.IsSelectedProperty, isSelected);
 
-        //        Panel.SetZIndex(boatControl, 1);
-        //        BoardGrid.Children.Add(boatControl);
-        //    }
-        //}
+                boatControl.SetBinding(BoatControl.BoatHeightProperty, height);
+                boatControl.SetBinding(BoatControl.BoatWidthProperty, width);
+                boatControl.SetBinding(ColumnProperty, column);
+                boatControl.SetBinding(RowProperty, row);
+                boatControl.SetBinding(ColumnSpanProperty, columnSpan);
+                boatControl.SetBinding(RowSpanProperty, rowSpan);
+                boatControl.SetBinding(BoatControl.IsSelectedProperty, isSelected);
+                boatControl.SetBinding(BoatControl.ShowBoatProperty, isEnabled);
+
+                Panel.SetZIndex(boatControl, 1);
+                BoardGrid.Children.Add(boatControl);
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
